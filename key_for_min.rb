b4484e3d680d87efd2e63e9f04a660d0dc5c4fc6
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-06-05 23:39:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   if name_hash.empty?
     nil
   elsif !name_hash.empty?
-    name_hash.collect do |name, value|
+    name_hash.each do |name, value|
       if value == 1 || value == 17
         name
       end
