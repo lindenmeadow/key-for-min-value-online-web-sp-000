53dbc37b273369232e452b0f991e39a88b137dfc
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-06-05 23:34:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,11 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.collect do |name, value|
-    if name_hash.empty?
-      nil
-    elsif value == 1 || value == 17
-      name
+  if name_hash.empty?
+    nil
+  elsif !name_hash.empty
+    name_hash.collect do |name, value|
+      if value == 1 || value == 17
+        name
     end
   end
 end
